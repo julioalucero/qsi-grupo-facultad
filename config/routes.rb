9cd71db8cfeeb9886d19db4fbe2ae3ef1d3b6581
Julio Lucero <poramo@gmail.com>
2013-10-13 18:57:30 -0300

    basic facebook authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 QsiGrupoFacultad::Application.routes.draw do
   root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
+
+  devise_for :users, :controllers => {
+    :registrations      => "registrations",
+    :omniauth_callbacks => "users/omniauth_callbacks"
+  }
+
   resources :users
 end
