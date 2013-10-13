7932797510a286895e28d9f40b6ce476af01aed0
Julio Lucero <poramo@gmail.com>
2013-10-13 16:20:59 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QsiGrupoFacultad::Application.config.secret_key_base = '01d122c2a76de8ae3ddcf35d285dbe147d6e72fa382bac0b922d46ae2546f2024d89d83a9bff24d5eef188d8414bd75240ecf282825e0fa4a18a88d757518dff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QsiGrupoFacultad::Application.config.session_store :cookie_store, key: '_qsi-grupo-facultad_session'
